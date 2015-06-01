bb58e0d49a0c3253de24db92191ae4e63c386e13
Rene Schneider <renews@oqvestir.com.br>
2015-06-01 10:45:41 -0300

    Removendo TABS

--- lib/generators/abacos_integration/install/install_generator.rb
+++ lib/generators/abacos_integration/install/install_generator.rb
@@ -1,16 +1,16 @@
 module AbacosIntegration
-	module Generators
+  module Generators
 
-		class InstallGenerator < Rails::Generators::Base
-			source_root File.expand_path('../templates', __FILE__)
+    class InstallGenerator < Rails::Generators::Base
+      source_root File.expand_path('../templates', __FILE__)
 
-			def generate_install
-				directory = File.expand_path('../templates', __FILE__)
+      def generate_install
+        directory = File.expand_path('../templates', __FILE__)
 
-				template "config/initializers/abacos_integration.rb", "config/initializers/abacos_integration.rb"
-			end
+        template "config/initializers/abacos_integration.rb", "config/initializers/abacos_integration.rb"
+      end
 
-		end
+    end
 
-	end
+  end
 end
