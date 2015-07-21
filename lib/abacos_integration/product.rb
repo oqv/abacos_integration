01ecd9ffd009ebdc982c1684d48e5e9d3640b7b9
Rene Schneider <renews@oqvestir.com.br>
2015-07-21 08:48:33 -0300

    Valida se o preço promocional veio com valor maior que zero.

--- lib/abacos_integration/product.rb
+++ lib/abacos_integration/product.rb
@@ -116,7 +116,11 @@ module AbacosIntegration
     def fetch_price(product_id)
       if ["1", "true", 1].include? AbacosIntegration.configuration.abacos_fetch_price.to_s
         if price = prices.find { |p| p[:codigo_produto] == product_id }
-          { price: price[:preco_promocional], cost_price: price[:preco_tabela] }
+          if price[:preco_promocional].to_i > 0
+            { price: price[:preco_promocional], cost_price: price[:preco_tabela] }
+          else
+            { price: price[:preco_tabela], cost_price: 0 }
+          end
         else
           { price: 0 , cost_price: 0}
         end
